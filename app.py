7b177253b8fed51a6593925192bf55a70a43acd7
grazicolaress <graziellecolaress@gmail.com>
2025-10-27 15:08:06 -0400

    feat: adicionada aula04 ao app.py com renderização de páginas HTML, removido aula04_paginas.py

--- app.py
+++ app.py
@@ -1,20 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template #nova biblioteca
 
-app_grazi = Flask (__name__)
+app_grazi = Flask (__name__, template_folder='t_templates')
 
-@app_grazi.route('/')
-@app_grazi.route('/rota1') 
-def rota1():
-    return 'Olá, turma!'
+@app_grazi.route('/') 
+def homepage():
+    return render_template ('homepage.html')
 
-@app_grazi.route('/rota2')
-def rota2():
-    resposta = "<h3> Essa é outra página da rota 2 <h3>"
-    return resposta 
-
-#@app_grazi.route('/ola/<nome>')
-def saudacoes (nome):       #agora essa função não pertence a nenhuma rota
-    return f'Olá, {nome}!'
+@app_grazi.route('/contato')
+def contato():
+    return render_template("contato.html")
 
 if __name__ == "__main__":  
     app_grazi.run(port=8880)
