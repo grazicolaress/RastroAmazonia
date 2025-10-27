c3931dec24dc5c77ae10b8284a50d1f2ce5ecfbd
grazicolaress <graziellecolaress@gmail.com>
2025-10-27 14:25:54 -0400

    feat: adicionado aula 03 ao app.py com rotas /rota1 e /rota2, removido aula03_rotas.porta.def.py

--- app.py
+++ app.py
@@ -3,13 +3,18 @@ from flask import Flask
 app_grazi = Flask (__name__)
 
 @app_grazi.route('/')
-@app_grazi.route('/ola') 
-def raiz():
+@app_grazi.route('/rota1') 
+def rota1():
     return 'Olá, turma!'
 
-@app_grazi.route('/ola/<nome>')
-def saudacoes (nome):       #função nova
+@app_grazi.route('/rota2')
+def rota2():
+    resposta = "<h3> Essa é outra página da rota 2 <h3>"
+    return resposta 
+
+#@app_grazi.route('/ola/<nome>')
+def saudacoes (nome):       #agora essa função não pertence a nenhuma rota
     return f'Olá, {nome}!'
 
-if __name__ == "__main__":  #verifica se o script está sendo executado diretamente    
+if __name__ == "__main__":  
     app_grazi.run(port=8880)
