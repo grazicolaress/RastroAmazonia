f2fde5f12c0e75848bf5bd0e332304f1ac639bc9
grazicolaress <graziellecolaress@gmail.com>
2025-10-27 16:10:51 -0400

    feat: adicionado aula05 com rotas dinâmicas /index e /usuario, removido aula05_paginas_dinamicas.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template #nova biblioteca
+from flask import Flask, render_template
 
 app_grazi = Flask (__name__, template_folder='t_templates')
 
@@ -8,7 +8,16 @@ def homepage():
 
 @app_grazi.route('/contato')
 def contato():
-    return render_template("contato.html")
+    return render_template('contato.html')
+
+@app_grazi.route('/index')
+def indice():
+    return render_template ('t_index.html', nome='Grazielle') #passa a variável nome para aparecer na tela
+
+@app_grazi.route('/usuario') #nova rota para perfil de usuario
+def dados_usuario():
+    dados_usu = {"nome":"Grazielle", "profissão":"Estagiária", "disciplina":"Desenvolvimento Web III"}
+    return render_template("t_usuario.html", dados = dados_usu)
 
 if __name__ == "__main__":  
     app_grazi.run(port=8880)
