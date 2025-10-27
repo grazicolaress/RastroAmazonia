51aab04ea3ea952686af4826959c7126cb873d9d
grazicolaress <graziellecolaress@gmail.com>
2025-10-27 19:25:24 -0400

    feat: adicionada aula06 com rotas dinâmicas /usuario/<id> e /usuario/<nome_usuario>;<nome_profissao>;<nome_disciplina>, e removido aula06_dinamico_URL.py

--- app.py
+++ app.py
@@ -12,12 +12,24 @@ def contato():
 
 @app_grazi.route('/index')
 def indice():
-    return render_template ('t_index.html', nome='Grazielle') #passa a variável nome para aparecer na tela
-
-@app_grazi.route('/usuario') #nova rota para perfil de usuario
+    return render_template ('t_index.html', nome='Grazielle') 
+                                        
+@app_grazi.route('/usuario')
 def dados_usuario():
-    dados_usu = {"nome":"Grazielle", "profissão":"Estagiária", "disciplina":"Desenvolvimento Web III"}
-    return render_template("t_usuario.html", dados = dados_usu)
+    dados_usu = {"nome": "Grazielle", "profissão": "Estagiária", "disciplina": "Desenvolvimento Web III"}
+    return render_template("t_usuario.html", dados=dados_usu)
+
+@app_grazi.route('/usuario/<id>')       #nova rota para exibir saudacao personalizada
+def saudacao(id):
+    return render_template('t_homepage_nome.html', nome=id)
+
+#acrescenta três parâmetros (nome_usuario, nome_profissao, nome_disciplina)
+@app_grazi.route('/usuario/<nome_usuario>;<nome_profissao>;<nome_disciplina>') 
+def usuario(nome_usuario, nome_profissao, nome_disciplina):
+    dados_usu = {"profissão": nome_profissao, "disciplina": nome_disciplina}
+    return render_template("t_usuario.html", nome=nome_usuario, dados = dados_usu)
 
 if __name__ == "__main__":  
     app_grazi.run(port=8880)
+    
+
