9121227cd10c24eab71bb853315d7ff0f51069a9
grazicolaress <graziellecolaress@gmail.com>
2025-10-27 13:47:10 -0400

    feat: adicionado rota /ola e função saudacoes ao app.py, removido aula02_rota.py

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ from flask import Flask
 app_grazi = Flask (__name__)
 
 @app_grazi.route('/')
+@app_grazi.route('/ola') 
 def raiz():
     return 'Olá, turma!'
 
-app_grazi.run()
+def saudacoes (nome):       #função nova
+    return f'Olá, {nome}!'
+
+if __name__ == "__main__":  #verifica se o script está sendo executado diretamente    
+    app_grazi.run(port=8880)
